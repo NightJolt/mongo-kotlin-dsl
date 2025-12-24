548efc5fd0e7f975ff08c53f3b532d2edebd7a08
nightjolt <mk.ff.dev@gmail.com>
2025-12-24 15:49:06 +0400

    npc activities

--- src/main/kotlin/com/glome/mongokotlindsl/UnwindBuilder.kt
+++ src/main/kotlin/com/glome/mongokotlindsl/UnwindBuilder.kt
@@ -1,11 +1,17 @@
 package com.glome.mongokotlindsl
 
+import kotlin.reflect.KProperty
+
 class UnwindBuilder {
     lateinit var field: String
     var preserveNullAndEmptyArrays: Boolean = false
 
     fun field(field: String) {
         this.field = field
+    }
+
+    fun field(field: KProperty<*>) {
+        this.field = field.propertyToMongoField()
     }
 
     fun preserveNullAndEmptyArrays(preserve: Boolean) {
